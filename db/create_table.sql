c8c9825fa57843426bbe68ec27e04804817ab152
JanniT <timoskainen.janni@gmail.com>
2023-03-01 15:10:15 +0200

    db: added university table

---
+++ db/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE University(
+    name VARCHAR(50)
+);
+
+--Insering text to the table
+INSERT INTO University VALUES
+    ("LUT University"),
+    ("Aalto University"),
+    ("University of Oulu"),
+    ("University of Vaasa"),
+    ("University of Eastern Finland");

a653ac3ac3774bf91865ad927513a01920bbe5ba
Leevi Laitala <leevi.laitala@student.lut.fi>
2023-03-09 13:59:20 +0200

    db: fix wrong num of columns in teacher table

--- db/create_table.sql
+++ db/create_table.sql
@@ -121,13 +121,13 @@ INSERT INTO University (name) VALUES
 
 --Inserting stuff to the Teacher table
 INSERT INTO Teacher VALUES
-    (1001, "Geoff", "Douglas", "02.05.1985", "geoff.douglas@jeejee.fi"),
-    (1002, "Anastasia", "Peterson", "13.05.1978", "anastasia.peterson@jeejee.fi"),
-    (1003, "Brad", "Baron","12.12.1986", "brad.barond@jeejee.fi"),
-    (1004, "Daniel", "Inder", "09.09.1967", "daniel.inder@jeejee.fi"),
-    (1005, "Troy", "Sable", "17.04.1988", "troy.sable@jeejee.fi"),
-    (1006, "Jennifer", "Frawley", "16.08.1981", "jennifer.frawley@jeejee.fi"),
-    (1007, "Monica", "Kaldor", "30.01.1989", "monica.kaldor@jeejee.fi");
+    (1001, "Geoff", "Douglas", "02.05.1985", "geoff.douglas@jeejee.fi", ""),
+    (1002, "Anastasia", "Peterson", "13.05.1978", "anastasia.peterson@jeejee.fi", ""),
+    (1003, "Brad", "Baron","12.12.1986", "brad.barond@jeejee.fi", ""),
+    (1004, "Daniel", "Inder", "09.09.1967", "daniel.inder@jeejee.fi", ""),
+    (1005, "Troy", "Sable", "17.04.1988", "troy.sable@jeejee.fi", ""),
+    (1006, "Jennifer", "Frawley", "16.08.1981", "jennifer.frawley@jeejee.fi", ""),
+    (1007, "Monica", "Kaldor", "30.01.1989", "monica.kaldor@jeejee.fi", "");
     
 --Inserting stuff to Class table
 INSERT INTO Class (name, room_ID, student_ID, teacher_ID) VALUES

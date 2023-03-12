a65ddeb95f40e6e46083a2c863782e0319e639d5
Janni <timoskainen.janni@gmail.com>
2023-03-12 18:05:54 +0200

    db: fixed the insert query

--- src/menufunction.py
+++ src/menufunction.py
@@ -145,11 +145,11 @@ def funcInsertStudent(db, cur):
     lastname = input("Write the last name: ")
     firstname = input("Write the first name: ")
     subject = input("Write the subject: ")
+    university = input("Write the university: ")
 
     query = "INSERT INTO Student VALUES"
     query += f"({id}, '{firstname}', '{lastname}', '{bd}', '{email}',"
-    query += f"'{subject}');"
+    query += f"'{subject}', '{university}');"
 
     cur.execute(query)
     db.commit()
-    return
